74b91d74a3657af463c382ae8039680c4915835e
Adam <adam@summers.com.tw>
2014-05-30 21:34:49 +0800

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -5,7 +5,9 @@ class ContactMailer < ActionMailer::Base
   def new_contact(contact)
   		@contact = contact
   		if !Admin.where(isnotify: true).pluck( :email ).empty?
-			mail( :to => Admin.where(isnotify: true).pluck( :email ), :subject => '收到了新的詢問！' ) 
+			mail( :to => Admin.where(isnotify: true).pluck( :email ), :subject => '收到了新的詢問！' ) do | format |
+	  			format.html { render 'notifytemplate' }
+	  		end
 	  	else
 	  		#if there is no one has been set  isnotify = true
 # 			mail( :to => Admin.pluck( :email ), :subject => '收到了新的詢問！' ) do | format |
