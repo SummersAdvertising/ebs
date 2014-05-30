072f6f080d444e3fe1cd2e589a9d343d66ac037a
Adam <adam@summers.com.tw>
2014-05-30 21:17:54 +0800

    fix

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,16 @@
 # encoding: utf-8
 class ContactMailer < ActionMailer::Base
-  default from: "\"Ebs\" <adword@summers.com.tw>"
+  default from: "<adword@summers.com.tw>"
   
   def new_contact(contact)
   		@contact = contact
   		if !Admin.where(isnotify: true).pluck( :email ).empty?
-			mail( :to => Admin.where(isnotify: true).pluck( :email ), :subject => '收到了新的詢問！' ) do | format |
-	  			format.html { render 'contact_mailer/new_contact' }
-	  		end
+			mail( :to => Admin.where(isnotify: true).pluck( :email ), :subject => '收到了新的詢問！' ) 
+	  	else
+	  		#if there is no one has been set  isnotify = true
+# 			mail( :to => Admin.pluck( :email ), :subject => '收到了新的詢問！' ) do | format |
+# 	  			format.html { render 'new_contact' }
+# 	  		end
 	  	end
   end
 end
